0391bef88970f717ddeb61eaf2105778d0a6e9f6
Sean-L <seanlavergne7@gmail.com>
2016-01-29 14:23:53 -0800

    Updated files for stage 3

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -125,6 +125,6 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('images');
 
 $autoload['helper'] = array('common', 'url');
