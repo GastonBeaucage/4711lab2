2ddf2de189d1fed38b9255326d6874be914c5eb3
Sean-L <seanlavergne7@gmail.com>
2016-01-29 15:52:34 -0800

    Finished Stage 3

--- application/models/Images.php
+++ application/models/Images.php
@@ -12,5 +12,21 @@ class Images extends CI_Model{
     {
         parent::__construct();
     }
+    
+    //return all images, descending order by post date
+    function all()
+    {
+           $this->db->order_by("id", "desc");
+           $query = $this->db->get('images');
+           return $query->result_array();
+    }
+    
+    function newest()
+    {
+        $this->db->order_by("id", "desc");
+        $this->db->limit(3);
+        $query= $this->db->get('images');
+        return $query->result_array();
+    }
 }
 
